6b0dff85d12eebcdd9c3a532de651bb8886246fa
FireFistDev <akaki7600@gmail.com>
2023-06-08 11:29:14 +0400

    updateAPI

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,14 @@ import { ConfigModule } from '@nestjs/config';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
 
+console.log('yler');
+
 @Module({
   imports: [
     ConfigModule.forRoot(),
-    MongooseModule.forRoot(process.env.MONGO_URI),
+    MongooseModule.forRoot(
+      'mongodb+srv://akaki7600:Hanway123@cluster5.xu4itzt.mongodb.net/',
+    ),
     UsersModule,
   ],
 })
